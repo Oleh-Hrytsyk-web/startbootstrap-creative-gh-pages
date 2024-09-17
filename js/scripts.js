2a98b5f5577f6af223dbc2b08c781c06e0944dc3
Oleh Hrytsyk <oleggrytsyk34@gmail.com>
2024-09-17 12:36:56 +0300

    add english lang

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ window.addEventListener('DOMContentLoaded', event => {
     // Navbar shrink function
     var navbarShrink = function () {
         const navbarCollapsible = document.body.querySelector('#mainNav');
+        
         if (!navbarCollapsible) {
             return;
         }
